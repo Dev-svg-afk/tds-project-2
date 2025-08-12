9eb82ea490a3a481982e8f74b5234a8ca4c74fef
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-08-12 11:36:43 +0530

    Stop tracking folders

--- tools/list_units.py
+++
@@ -1,6 +0,0 @@
-def listUnits():
-  with open("../response.txt", "r", encoding="utf-8") as resp_file:
-      response_parts = resp_file.read().split('\n\n')
-  print(response_parts[3])
-
-listUnits()
