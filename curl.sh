fb2f41161339652bd3d7102d82a5c6aa93109337
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-08-12 11:37:35 +0530

    stop tracking curl.sh

--- curl.sh
+++
@@ -1,3 +0,0 @@
-# curl -X POST "http://localhost:8000/api" -F "questions.txt=@questions/question2.txt"
-
-curl -X POST "http://localhost:8000/api" -F "questions.txt=@questions/question3/questions.txt" -F "sample-sales.csv=@questions/question3/sample-sales.csv"
